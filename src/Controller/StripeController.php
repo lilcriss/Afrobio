1803232182f4a24f6efb4b9a14e7d1fbcf6f43f7
lilcriss <lilcriss@hotmail.fr>
2022-01-04 01:48:11 +0100

    stripe: change adress of site domaine

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -23,7 +23,7 @@ class StripeController extends AbstractController
     public function index(EntityManagerInterface $entityManager, OrderRepository $orderRepository,ProductRepository $productRepository, Cart $cart, $reference)
     {
         $product_for_stripe = [];
-        $YOUR_DOMAIN = 'http://127.0.0.1:8000';
+        $YOUR_DOMAIN = 'https://afrobio.criss-dev.fr/';
 
         $order = $orderRepository->findOneBy(['reference' => $reference]);
 
